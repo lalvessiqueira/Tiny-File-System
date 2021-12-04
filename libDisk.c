bec4033c6c901ec5eb61e9abe0a9f5df8ee6ec11
Leticia Siqueira <lalvessi@calpoly.edu>
2021-12-04 14:06:37 -0800

    functions all written
    just need to check them later on!
    check if the paramenters are valid

--- libDisk.c
+++ libDisk.c
@@ -3,12 +3,16 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <stdlib.h>
+#include "libDisk.h"
+
  /* The first part of the assignment is to build a 
  disk emulator, or more generically a “block device 
  emulator”. You will implement an emulator that will 
  accomplish basic block operations, like the kind supported 
  by block devices (e.g. hard disk drives), on a regular 
  Unix file.*/
+
+ #define EXIT_FAILURE -1
 
 int main() {
     printf("Hello World!");
@@ -34,20 +38,55 @@ int openDisk(char *filename, int nBytes) {
             return EXIT_FAILURE;
         }
 
+        for (i = 0; i < nBytes; i++) {
+            int ret = write(fd, "\0", 1);
+            if(ret == -1) {
+                return EXIT_FAILURE;
+            }
+        }
     }
-
-
-
+    return fd;
 }
 
 int closeDisk(int disk) {
-
+    // do I need to check for disk?
+    int ret = close(disk);
+    if (ret == -1) {
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
 
 int readBlock(int disk, int bNum, void *block) {
+// check for parameters
 
+    // lseek write it on another local buffer
+    int b_size = bNum * BLOCKSIZE;
+    int ret = lseek(disk, b_size, SEEK_SET);
+    // check if it worked correctly
+    if (ret != b_size) {
+        return EXIT_FAILURE;
+    }
+    ret = read(disk, block, BLOCKSIZE);
+    // check
+    if (ret == -1) {
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
 
 int writeBlock(int disk, int bNum, void *block) {
-
+    // lseek write it on another local buffer
+    int b_size = bNum * BLOCKSIZE;
+    int ret = lseek(disk, b_size, SEEK_SET);
+    // check if it worked correctly
+    if (ret != b_size) {
+        return EXIT_FAILURE;
+    }
+    ret = write(disk, block, BLOCKSIZE);
+    // check
+    if (ret == -1) {
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
